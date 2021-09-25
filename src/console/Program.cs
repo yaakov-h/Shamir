ec19c1d01af66909a1606e5928b8d77185c5b307
Yaakov <git@yaakov.dev>
2021-09-25 21:20:48 +1000

    Initial project structure

---
+++ src/console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shamir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
